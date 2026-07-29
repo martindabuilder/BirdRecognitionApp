c1e0e3b88fde2cd7bc9d4d7c916507320b2f90e8
Martin Chalakov <toxzon9@gmail.com>
2026-07-29 22:22:43 +0300

    More setup and some changes

--- backend/BackendProgress.md
+++ backend/BackendProgress.md
@@ -1,1 +1,10 @@
 File containing all the references and progress on the creation of the backend portion of the project.
+
+The original 2 datasets used fior the initial 500 classes when creating the project are:
+[Xeno-Canto Bird Recordings Extended (A-M)](https://www.kaggle.com/datasets/rohanrao/xeno-canto-bird-recordings-extended-a-m)
+[Xeno-Canto Bird Recordings Extended (N-Z)](https://www.kaggle.com/datasets/rohanrao/xeno-canto-bird-recordings-extended-n-z)
+Later on the projects lifecycle, the dataset will be expanded with more species
+
+Considering the N-Z collection's CSV file is wrong from the Kaggle download, ill be using the eBird taxonomy CSV file for the corresponding bird class to the code from the xeno canto  [eBird Taxonomy v2025](https://www.birds.cornell.edu/clementschecklist/introduction/updateindex/october-2025/2025-citation-checklist-downloads/)
+
+For the corresponding bird classes to be able to add photos/location ill be using the iNaturalist endpoint so i can access the photos, but that will come later on
--- backend/audio-processing.py
+++ backend/audio-processing.py
@@ -9,21 +9,21 @@ from sklearn.preprocessing import LabelEnconder
 
 warnings.filterwarnings("ignore")
 
-#folders conainting the npy output of the spectrograms and the images themself
-spectro_img_out = "spectrogram_images"
+#folders conainting the npy output of the spectrograms
 spectogram_npy_out = "spectrograms_npy"
 
 #creating (if missing) folders
-os.makedirs(spectro_img_out, exist__ok = True)
 os.makedirs(spectogram_npy_out, exist__ok = True)
 
 #spectrogram specifications
-mel_lines = 150
-spectro_width = 128
-sample_rate = 16000
-audio_duration = 5.0
-minimal_active_threshold = 0.5
-max_darkness = 0.10
+mel_lines = 150 #spectrogram height, measured in n_mels
+spectro_width = 128 #spectrogram width
+sample_rate = 16000 
+audio_duration = 5.0 #seconds per segment
+minimal_active_threshold = 0.5 #the amount of "active" volume in a single column
+max_darkness = 0.10 #mean intensity below 0.10 means the segment gets skipped as it is not active enough
 
-x = [] #spectrograms
-y = [] #all seperate bird classes
+#storage dtype for the saved spectrograms
+#a float16 format is chosen as it is a healthy middleground between uint8 and float36
+
+
