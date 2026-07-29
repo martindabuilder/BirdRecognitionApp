6ae59752c3f1eaab59a832252ed2270ae62a4009
Martin Chalakov <toxzon9@gmail.com>
2026-07-29 23:32:56 +0300

    working on the split_audio func
    Adding the first function that splits the audio files into 5sec segments

--- backend/BackendProgress.md
+++ backend/BackendProgress.md
@@ -1,5 +1,6 @@
 File containing all the references and progress on the creation of the backend portion of the project.
 
+---- audio-processing file references ----
 The original 2 datasets used fior the initial 500 classes when creating the project are:
 [Xeno-Canto Bird Recordings Extended (A-M)](https://www.kaggle.com/datasets/rohanrao/xeno-canto-bird-recordings-extended-a-m)
 [Xeno-Canto Bird Recordings Extended (N-Z)](https://www.kaggle.com/datasets/rohanrao/xeno-canto-bird-recordings-extended-n-z)
@@ -8,3 +9,11 @@ Later on the projects lifecycle, the dataset will be expanded with more species
 Considering the N-Z collection's CSV file is wrong from the Kaggle download, ill be using the eBird taxonomy CSV file for the corresponding bird class to the code from the xeno canto  [eBird Taxonomy v2025](https://www.birds.cornell.edu/clementschecklist/introduction/updateindex/october-2025/2025-citation-checklist-downloads/)
 
 For the corresponding bird classes to be able to add photos/location ill be using the iNaturalist endpoint so i can access the photos, but that will come later on
+
+Deciding on the amount of used cores for preprocessing - [How the "Number of Workers" Parameter in PyTorch DataLoader Actually Works](https://www.geeksforgeeks.org/deep-learning/how-the-number-of-workers-parameter-in-pytorch-dataloader-actually-works/)
+
+res_type(soxr_hq is chosen) used with the librosa library used for the resampling [librosa.resample](https://librosa.org/doc/main/generated/librosa.resample.html). Its high quality ( second highest behind VHQ, and is also much faster for the resampling as we are handling 500+ classes)
+
+---- model training file references ----
+
+---- backend file references ----
--- backend/audio-processing.py
+++ backend/audio-processing.py
@@ -4,26 +4,61 @@
 import os
 import librosa
 import numpy as np
-from matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEnconder
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from multiprocessing import Pool, cpu_count
 
+import warnings
 warnings.filterwarnings("ignore")
 
 #folders conainting the npy output of the spectrograms
-spectogram_npy_out = "spectrograms_npy"
+spectogram_npy_output = "spectrograms_npy"
 
 #creating (if missing) folders
-os.makedirs(spectogram_npy_out, exist__ok = True)
+os.makedirs(spectogram_npy_output, exist_ok = True)
 
 #spectrogram specifications
 mel_lines = 150 #spectrogram height, measured in n_mels
-spectro_width = 128 #spectrogram width
-sample_rate = 16000 
+spectrogram_width = 128 #spectrogram width
+sample_rate = 16000 #set sample rate for each spectrogram
 audio_duration = 5.0 #seconds per segment
-minimal_active_threshold = 0.5 #the amount of "active" volume in a single column
-max_darkness = 0.10 #mean intensity below 0.10 means the segment gets skipped as it is not active enough
+minimal_active_threshold = 0.5 #the amount of "active" volume in the file
+max_darkness = 0.10 #intensity below 0.10 means the segment gets skipped, it's not active enough (i.e its too quiet/empty)
 
 #storage dtype for the saved spectrograms
 #a float16 format is chosen as it is a healthy middleground between uint8 and float36
+STORAGE_DTYPE = "float16"
 
+#i have chosen to use 4 of the available cores on my computer for the processing
+#in case you want to test and run this locally, never go above the amount you actually have
+#(i would even suggest your cores - 1 just in case)
+NUM_WORKERS = min(4, cpu_count())
 
+#used to precompute the lenght of the spectrogram, removing the need of resizing
+segment_samples = int(audio_duration * sample_rate)
+hop_length = segment_samples // spectrogram_width
+
+def split_audio(file_path, sr = sample_rate, duration = audio_duration):
+    y, sr = librosa.load(file_path, sr = sr, res_type = "soxr_hq")
+    y, _ = librosa.effects.trim(y)
+    segment_lentgh = int(duration * sr)
+    segments = []
+
+    #this segment of the function is used in case a segment is too short
+    #the empty time is filled up with 0s, until the segment reaches a 5s length
+    for start in range(0, len(y), segment_length):
+        end = start + segment_length
+        seg = y[start:end]
+        if len(seg) < segment_length:
+            seg = np.pad(seg, (0, segment_length - len(seg)))
+        segments.append(seg)
+    return segments, sr
+
+def audio_to_spectrogram():
+    pass
+
+def is_seg_active():
+    pass
+
+def spectrogram_too_dark():
+    pass
