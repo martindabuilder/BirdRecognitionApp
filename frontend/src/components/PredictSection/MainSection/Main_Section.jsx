dcbeb74b2e37bc3a5e06aa34b965f59d708faf01
Martin Chalakov <toxzon9@gmail.com>
2026-08-26 00:42:27 +0300

    Designing and shaping the frontend
    Made some concept designs for what i want the design to look like, working on implementing them now.

--- frontend/src/components/PredictSection/MainSection/Main_Section.jsx
+++ frontend/src/components/PredictSection/MainSection/Main_Section.jsx
@@ -1,12 +1,15 @@
 import "./main_section.css"
 
+import TitleBar from "../TitleBar/TitleBar.jsx"
+
 import MicrophonePredictButton from "../MicrophonePredictButton/Microphone_Predict_Button.jsx"
 import PredictButton from "../PredictButton/Predict_Button.jsx"
+
 
 function MainSection(){
     return(
         <section className = "main-section">
-            <p> Birds yaaayyy </p>
+            <TitleBar />
             <PredictButton />
             <MicrophonePredictButton />
         </section>
---
+++ frontend/src/components/PredictSection/TitleBar/TitleBar.jsx
@@ -0,0 +1,11 @@
+import "./title-bar.css"
+
+function TitleBar(){
+    return (
+        <div className = "title-bar">
+            Birds Recognition Project
+        </div>
+    )
+}
+
+export default TitleBar
