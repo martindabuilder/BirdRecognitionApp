c0bc2a9ccfbded40dca247d937885792f80c086a
Martin Chalakov <toxzon9@gmail.com>
2026-08-30 00:49:15 +0300

    Added proper navigation between the sections

--- frontend/src/components/BirdListSection/Bird_Section.jsx
+++ frontend/src/components/BirdListSection/Bird_Section.jsx
@@ -1,11 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
 import BirdPhoto from "../Shared/BirdPhoto.jsx"
 
 import "./bird_section.css"
 
 function BirdList(){
+    const navigate = useNavigate()
     return (
         <section className="list-section">
-            atesh
+            <button className = "back-button" onClick = {() => navigate("/")}>
+                go back home
+            </button>
+            bird list yippee
         </section>
     )
 }
--- frontend/src/components/InfoSection/Info_Section.jsx
+++ frontend/src/components/InfoSection/Info_Section.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 import "./info_section.css"
 
 
 function InfoSection(){
+    const navigate = useNavigate()
     return (
         <section className="info-section">
-            atesh
+            <button className = "back-button" onClick = {() => navigate("/")}>
+                go back home
+            </button>
+            ifnromation 
         </section>
     )
 }
