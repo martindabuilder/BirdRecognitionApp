4abc9883c2ba8adc76a412343d249fa8704113be
Martin Chalakov <toxzon9@gmail.com>
2026-08-30 15:16:43 +0300

    Photo extraction feature
    The project now gets high quality photos for the corresponding bird.

--- FrontendReferencess.md
+++ FrontendReferencess.md
@@ -1,22 +1,25 @@
 Frontend will be developed with ReactJS
 
+
 References used when creating this part of the project
 
 ---- STRUCTURING ----
-
 [Best Practices for Creating a Folder and File Structure for a React Application](https://medium.com/@wolfflucas/best-practices-for-creating-a-folder-and-file-structure-for-a-react-application-82b6550b3bbf)
-
 [Why to use separate CSS files for components in React.js](https://stackoverflow.com/questions/60464799/why-to-use-separate-css-files-for-components-in-react-js)
 
 
 ---- OVERFLOW ----
 [A Simple Fix for Your CSS Overflow Problems](https://www.youtube.com/watch?v=2WWAEftnzf0)
 
+
 ---- TEXT RELATED REFERENCES ----
 https://stackoverflow.com/questions/4919076/outline-effect-to-text
 
+
 ---- BIRD PHOTOS ----
-[Wikimedia commons](https://commons.wikimedia.org/wiki/Main_Page) is used to fetch the corresponding bird photos, without the need to download naything locally.
+[Wikimedia commons](https://commons.wikimedia.org/wiki/Main_Page), particulary their [API](https://www.mediawiki.org/wiki/API:Imageinfo) is used to fetch the corresponding bird photos, without the need to download naything locally.
+
+
 
 for potential future use
 [GBIF API reference](https://techdocs.gbif.org/en/openapi/)
@@ -25,3 +28,4 @@ for potential future use
 
 ---- THINGS TO PUT IN THE SECTION OF LIST OF SOUCES ----
 [Wikimedia commons](https://commons.wikimedia.org/wiki/Main_Page)
+[Wikimedia commons API info](https://www.mediawiki.org/wiki/API:Imageinfo)
--- frontend/src/components/Shared/BirdPhoto.jsx
+++ frontend/src/components/Shared/BirdPhoto.jsx
@@ -1,28 +1,62 @@
 import { useEffect, useState } from "react";
 
+import fallBackImage from "../../assets/fallbackimage.jpg"
 import "./bird_photo.css";
 
 function BirdPhoto({ commonName, scientificName }) {
     const [photo, setPhoto] = useState(null)
-    const [loading, setLoading] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         async function getBirdPhoto() {
             setPhoto(null)
-            setLoading(null)
+            setLoading(true)
 
             try {
                 const searchBird = scientificName || commonName
+                const params = new URLSearchParams({
+                    action: "query",
+                    generator: "search",
+                    gsrsearch: searchBird,
+                    gsrnamespace: "6",
+                    gsrlimit: "10",
+                    prop: "imageinfo",
+                    iiprop: "url|mime|size",
+                    iiurlwidth: "800",
+                    format: "json",
+                    origin: "*"
+                })
 
-                const params = new URLSearchParams({})
-            }
-            catch (error) {
-                
-            }
+                const response = await fetch(`https://commons.wikimedia.org/w/api.php?${params}`)
+                if (!response.ok) {throw new Error("Failed to search Wikimedia Commons")}
+
+                const data = await response.json()
+                const pages = Object.values(data.query?.pages || {})
+                const image = pages.find(page => {
+                    const info = page.imageinfo?.[0]
+                    if (!info?.thumburl || !info.mime?.startsWith("image/")) { return false }
+                    const aspectRatio = info.width / info.height
+                    return aspectRatio >= 0.55 && aspectRatio <= 1.55
+                })
+
+
+                if (image) {setPhoto((image.imageinfo)[0].thumburl)}
+            } 
+            catch (error) { } 
+            finally { setLoading(false) }
         }
-        
-    })
+        if (commonName || scientificName) { getBirdPhoto() } 
+    }, [commonName, scientificName])
 
+    if (loading) {return ( <div className = "loading"> Loading photo... </div>)}
+    if (!photo) { return ( <img className="bird-photo" src={fallBackImage} alt="No bird photo available"/>)}
+
+    /* returns the corresponding image */
+    return (
+        <img className = "bird-photo" 
+        src = {photo} 
+        alt = {commonName} />
+    )
 }
 
 export default BirdPhoto;
